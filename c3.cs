3e7479c068037d15d358b3687efa24ead02269d8
Italog1812 <48763486+Italog1812@users.noreply.github.com>
2019-04-10 12:06:36 -0300

    Add files via upload
    Imcompleto

---
+++ c3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication13
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int p=1,m, c1, c2, c3,  b,n;
+            Console.WriteLine("Quantas pessoas irão votar?");
+            m = int.Parse(Console.ReadLine());
+            Console.WriteLine("Para votar em João 1, José 2, Maria 3, Branco 4, Nulo5");
+            while (p<=m)
+            {
+                p++;
+                Console.WriteLine("Digite o seu voto");
+                int voto = int.Parse(Console.ReadLine());
+                if                   
+            }
+        }
+
+    }
+}
