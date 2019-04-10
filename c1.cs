85747ce5a1bb3967da4068a5528b00c64115a1fc
Italog1812 <48763486+Italog1812@users.noreply.github.com>
2019-04-10 11:14:47 -0300

    Add files via upload

---
+++ c1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double temperatura, media=0, maior, menor, n, i, soma=0;
+            Console.WriteLine("Quantas temperaturas serão digitadas?");
+            n = double.Parse(Console.ReadLine());
+            Console.Write("Digite uma teperatura: ");
+            temperatura = double.Parse(Console.ReadLine());
+            menor = temperatura;
+            maior = temperatura;
+            soma += temperatura;
+            for (i = 1; i < n; i++)
+            {
+                Console.Write("Digite uma teperatura: ");
+                temperatura = double.Parse(Console.ReadLine());
+                soma += temperatura;
+                if (temperatura < menor)
+                {
+                    menor = temperatura;
+                }
+                else if (temperatura>maior)
+                {
+                    maior = temperatura;
+                }
+            }
+            media = soma / i;
+            Console.WriteLine("A menor temperatura é {0} e a maior é {1}, a média da temperatura é {2}",menor,maior,media);
+            Console.ReadKey();
+        }
+    }
+}
